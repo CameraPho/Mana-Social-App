d548bd22326f38bcf7fd426dc9dc3e4f31af87cb
CameraPho <camerapho@gmail.com>
2026-05-21 08:55:04 -0700

    Delete parsers directory

--- parsers/chaseSapphirePDF.ts
+++
@@ -1,6 +0,0 @@
-import type { ParseResult } from '@/lib/types'
-import { parseChaseConsumerPDF } from './amazonChasePDF'
-
-export async function parseChaseSapphirePDF(file: File): Promise<ParseResult> {
-  return parseChaseConsumerPDF(file, 'Chase Sapphire Preferred')
-}
